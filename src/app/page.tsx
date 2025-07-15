ab05a6b79bf62d48bb011e665e1d38f225e920b4
Nastaran <farjamiinastaran@gmail.com>
2025-07-15 10:12:28 +0330

    change some files, i will fix it today...

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-black">
-      <h1 className="text-5xl font-bold text-pink-500">Tailwind Works!</h1>
-    </main>
-  );
-}
