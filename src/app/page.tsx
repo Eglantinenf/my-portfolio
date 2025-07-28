0a62f2ba265a7631429c90feb7ad04c13fc94dc9
Nastaran <farjamiinastaran@gmail.com>
2025-07-28 15:56:24 +0330

    minor update for about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import AboutSection from "@/components/About/AboutSection";
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col">
       <Navbar />
       <Hero name="Nastaran Farjami" />
+      <AboutSection />
     </main>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -36,7 +36,7 @@ const Hero: React.FC<HeroTypes> = ({ name }) => {
   ]);
 
   return (
-    <section className="h-screen flex flex-col justify-center items-center hero-overlay relative px-4 md:px-6">
+    <section className="h-screen flex flex-col justify-center items-center section-bg relative px-4 md:px-6">
       <AnimatedBackground />
       <div className="flex flex-col lg:flex-row items-center gap-6 md:gap-8 max-w-6xl w-full">
         <div className="relative group">
