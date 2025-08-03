2bb3da2864463eb1965da7efcbeeb749ba721c91
Nastaran <farjamiinastaran@gmail.com>
2025-08-03 13:52:17 +0330

    trying to add Tilt effect with framer motion(without libraries)

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { motion } from "framer-motion";
+import { useMotionValue, useTransform, motion } from "framer-motion";
+import { useRef } from "react";
 import type { Project } from "./ProjectsData";
 import { Github, ExternalLink } from "lucide-react";
 
